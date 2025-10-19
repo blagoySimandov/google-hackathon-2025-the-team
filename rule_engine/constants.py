fef9139c7294c6dcbc243cad13590af1f86eb48a
blagoySimandov <baggy.simandoff@gmail.com>
2025-10-19 22:55:10 +0100

    feat: save the data into  a new collection  as well

--- rule_engine/constants.py
+++ rule_engine/constants.py
@@ -1,9 +1,18 @@
 from enum import Enum
 
 COLLECTION_NAME = "properties"
+COLLECTION_NAME_VALIDITY_DATA = "validity_data"
 
 
-class FIELD_NAMES(Enum):
+# Field names from the rules engine
+class FIELD_NAMES_RE(Enum):
     VALIDITY_SCORE = "validity_score"
     COMMUNITY_SCORE = "community_score"
+    ID = "id"
+
+
+# Field names for firestore
+class FIELD_NAMES_FE(Enum):
+    VALIDITY_SCORE = "validityScore"
+    COMMUNITY_SCORE = "communityScore"
     ID = "id"
